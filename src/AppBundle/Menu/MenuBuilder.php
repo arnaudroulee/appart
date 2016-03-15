79386afc5417b1ee31ae862100d2af13e5434b82
Arnaud <arnaud.roulee@gmail.com>
2016-03-15 08:34:49 +0100

    Update Menu + listing

--- src/AppBundle/Menu/MenuBuilder.php
+++ src/AppBundle/Menu/MenuBuilder.php
@@ -19,11 +19,23 @@ class MenuBuilder
 
     public function createMainMenu(Request $request)
     {
-       $menu = $this->factory->createItem('root');
+        $menu = $this->factory->createItem('root');
+
         $menu->setChildrenAttribute('class', 'nav navbar-nav');
-        
-        $menu->addChild('Se connecter', array('route' => 'fos_user_security_login', 'class' => 'nav-right'));
-        
+
+        $menu->addChild('Recherche', array('route' => 'locations_index'));
+
+        return $menu;
+    }
+
+    public function createUserMenu(Request $request)
+    {
+        $menu = $this->factory->createItem('root');
+
+        $menu->setChildrenAttribute('class', 'nav navbar-nav pull-right');
+
+        $menu->addChild('Se connecter', array('route' => 'fos_user_security_login'));
+
         $menu->addChild('S\'inscrire', array('route' => 'fos_user_registration_register'));
         // ... ajoutez ici les autres liens de base
 
